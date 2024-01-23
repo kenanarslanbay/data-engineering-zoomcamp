69d4cb6117ca650cda793ca77df9a9b0a580dcf6
Kenan Arslanbay <Kenan@data-engineering-zoomcamp.europe-west1-b.c.semiotic-pager-411512.internal>
2024-01-23 10:58:20 +0000

    added terraform files

--- 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/main.tf
+++ 01-docker-terraform/1_terraform_gcp/terraform/terraform_with_variables/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "5.6.0"
+      version = "5.12.0"
     }
   }
 }
@@ -10,15 +10,13 @@ terraform {
 provider "google" {
   credentials = file(var.credentials)
   project     = var.project
-  region      = var.region
+  region      = var.location
 }
 
-
-resource "google_storage_bucket" "demo-bucket" {
+resource "google_storage_bucket" "my-demo-bucket" {
   name          = var.gcs_bucket_name
   location      = var.location
   force_destroy = true
-
 
   lifecycle_rule {
     condition {
@@ -29,8 +27,6 @@ resource "google_storage_bucket" "demo-bucket" {
     }
   }
 }
-
-
 
 resource "google_bigquery_dataset" "demo_dataset" {
   dataset_id = var.bq_dataset_name
